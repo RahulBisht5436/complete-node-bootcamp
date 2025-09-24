22eb3c23c30f386a7ed08b8a99f1da92fdad42f6
Rahul bisht <Rahulbisht5436@gmail.com>
2025-09-24 06:49:12 +0530

    template rendering

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,16 +4,46 @@ let readError = false
 let fileJsonData = null
 try {
     fileJsonData = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
-    console.log(fileJsonData, "uasusdstudyat")
 } catch (err) {
     readError = true
     const errorMessage = err.message
     console.log('Error reading file:', errorMessage)
 }
+
+const templateOverviewData = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const templateProductData = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
+const templateCardData = fs.readFileSync(`${__dirname}/starter/templates/template-product-card.html`, 'utf-8');
+const productData = JSON.parse(fileJsonData);
+function replaceTemplate(template, product) {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    // Handle organic/non-organic
+    if (!product.organic) {
+        output = output.replace(/{%NOTORGANIC%}/g, 'not-organic');
+    } else {
+        output = output.replace(/{%NOTORGANIC%}/g, '');
+    }
+    return output;
+}
+
+
 const server = http.createServer((req, res) => {
 
+
+
+
+    
     if(req.url === '/' || req.url === '/overview'){
-        res.end('This is the overview')
+        res.writeHead(200,{'Content-type': 'text/html'})
+        const cards = productData.map(el => replaceTemplate(templateCardData, el)).join("");
+        const overviewHTML = templateOverviewData.replace('{%PRODUCTCARD%}', cards);
+        res.end(overviewHTML)
     }
     else if(req.url === '/product'){
         res.end('This is the product')
@@ -26,7 +56,6 @@ const server = http.createServer((req, res) => {
                 return
             }
             
-            const productData = JSON.parse(fileJsonData)
             res.writeHead(200, { 'Content-type': 'application/json' })
             res.end(JSON.stringify(productData))
         
