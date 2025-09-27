a2062552994dadc9d45b41e2a54a09709545ca1e
Rahul bisht <Rahulbisht5436@gmail.com>
2025-09-27 10:27:34 +0530

    changes for the event loop

---
+++ 2-how-node-works/starter/eventLoop.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+console.log('Hello from the top-level code of eventLoop.js')
+
+setTimeout(() => console.log('0 sec timer'), 0)
+setImmediate(() => console.log('Immediate timer'))
+
+fs.readFile('starter/test-file.txt', 'utf-8', (err, data) => {  
+    console.log('I/O finished')
+    }
+)
