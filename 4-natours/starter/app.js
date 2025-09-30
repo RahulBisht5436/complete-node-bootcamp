f11d1ff0671121d3018e1160d7da32f3a5695772
Rahul bisht <Rahulbisht5436@gmail.com>
2025-09-30 23:30:23 +0530

    env variable fix

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,11 +4,12 @@ const app = express();
 const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
+app.use(express.static(`${__dirname}/public`))
 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
 
-
-
-app.use(morgan('dev'))
 app.use(express.json());
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
-const port = 3000;
+
+// LEARN : this is new way to set environment variables
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env'  });
+
+const port = process.env.PORT || 3000;
+
+console.log(app.get('env'));
+console.log(process.env)
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
