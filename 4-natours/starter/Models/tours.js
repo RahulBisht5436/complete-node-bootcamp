4c39f0126d1c5f195584719780ddeb3c85844202
Rahul bisht <Rahulbisht5436@gmail.com>
2025-10-12 11:01:00 +0530

    changes for DB

--- 4-natours/starter/Models/tours.js
+++ 4-natours/starter/Models/tours.js
@@ -10,7 +10,7 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a duration']
     },
-    maxFroupSize: {
+    maxGroupSize: {
         type: Number,
         required: [true, 'A tour must have a group size']   
     },
@@ -32,8 +32,25 @@ const tourSchema = new mongoose.Schema({
     priceDiscount: Number,
     summary: {
         type: String,
-        trim: true
+        required: [true, 'A tour must have a summary'],
+        trim: true,
       },
+    description: {
+        type: String,
+        trim: true
+        },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']  
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),    
+    },
+    startDates: [Date]
+
+    
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
