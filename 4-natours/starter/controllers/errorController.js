33e1e389120a3fa851ccf4c455861b7fd5aa0d1f
Rahul bisht <Rahulbisht5436@gmail.com>
2025-11-06 22:09:34 +0530

    changes for the errors

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,8 +1,29 @@
+const sendErrorDev = (err, req, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+const sendErrorProd = (err, req, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    })
+}
+
 module.exports = (err, req, res, next) => {
-    err.status=err.statusCode || 500;
-    err.status=err.status || 'error';
+    err.status = err.statusCode || 500;
+    err.status = err.status || 'error';
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, req, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, req, res);
+    }
     res.json({
         status: err.status,
         message: err.message
     })
-}
+} 
