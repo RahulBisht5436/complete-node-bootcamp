581b879bc2416f0170b3e51dc91bce62785e7090
Rahul bisht <Rahulbisht5436@gmail.com>
2025-11-07 06:57:35 +0530

    handling the exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,17 @@
+// NOTE : handling uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+
 const app = require('./app')
 const mongoose = require('mongoose');
 // LEARN : this is new way to set environment variables
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
 const DB = process.env.DATABASE.replace(
     '<db_password>',
     process.env.DATABASE_PASSWORD
@@ -21,8 +30,16 @@ mongoose.connect(DB, {
         console.log('DB connection failed!');
     });
 
-
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
+// NOTE unhandled rejection handling globally
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+});
+
