a7c3c338fd0b165ed3782a93e7c41c888d496c25
Rahul bisht <Rahulbisht5436@gmail.com>
2025-11-09 16:52:46 +0530

    added json token with the user

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,12 +1,25 @@
 const User = require('./../Models/User');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken')
+
 
 const signup = catchAsync(async function signup(req, res, next) {
-    const newUser = await User.create(req.body);
-    console.log(req.body)
+    // const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        password_confirmed: req.body.password_confirmed
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+
     // send single response
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
