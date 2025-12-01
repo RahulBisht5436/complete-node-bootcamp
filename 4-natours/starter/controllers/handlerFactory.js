96fa703f5b5ac51a38b21d36096f35cdafec117f
Rahul bisht <Rahulbisht5436@gmail.com>
2025-12-01 18:47:22 +0530

    delete Factory Handler

---
+++ 4-natours/starter/controllers/handlerFactory.js
@@ -0,0 +1,20 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const deleteOne = Model => catchAsync(async (req, res, next) => {
+
+    const doc = await Model.findByIdAndDelete(id);
+
+    if (!doc) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
+    res.status(204).json({
+        status: 'success',
+        data: doc
+    });
+
+
+})
+
+module.exports = { deleteOne }
+
